651abed62fa7b34954ec45e848127716b8dfbc53
Prakhar <prakharb56@gmail.com>
2023-03-27 23:10:26 +0530

    renamed project and cleared unnecessary files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tasker from './components/Tasker';
+
+function App() {
+  return (
+    <div className="App">
+      <Tasker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasker.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tasker() {
+  return (
+    <>
+   <h3>Tasker</h3>
+   <button>Create Task</button>
+   </>
+  )
+}
