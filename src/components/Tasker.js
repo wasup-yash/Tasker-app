cce52fac28723103bdd837096d87d65b96b8ebf2
Prakhar <prakharb56@gmail.com>
2023-03-28 03:18:52 +0530

    building task creator

--- src/components/Tasker.js
+++ src/components/Tasker.js
@@ -3,8 +3,13 @@ import React from 'react'
 export default function Tasker() {
   return (
     <>
+    <div className='header text-center'>
    <h3>Tasker</h3>
-   <button>Create Task</button>
+   <button className='btn btn-primary mt-2'>Create Task</button>
+    </div>
+    <div className="task-container">
+
+    </div>
    </>
   )
 }
