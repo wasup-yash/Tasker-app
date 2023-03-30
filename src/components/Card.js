639fead5bb34e6337e33f01a9afb649c850e1263
Prakhar <prakharb56@gmail.com>
2023-03-30 13:15:32 +0530

    fixing css issues

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,50 +1,81 @@
-import React, { useState } from 'react'
+import React from "react";
 
-import Edittask from './Edittask'
+import Edittask from "./modal/Edittask";
 
-export default function Card({taskObj , index , taskList , setTaskList , setModal , modal , toggle , deleteTask}) {
-    
-    const colors = [
-        {
-            primaryColor : "#5D93E1",
-            secondaryColor : "#ECF3FC"
-        },
-        {
-            primaryColor : "#F9D288",
-            secondaryColor : "#FEFAF1"
-        },
-        {
-            primaryColor : "#5DC250",
-            secondaryColor : "#F2FAF1"
-        },
-        {
-            primaryColor : "#F48687",
-            secondaryColor : "#FDF1F1"
-        },
-        {
-            primaryColor : "#B964F7",
-            secondaryColor : "#F3F0FD"
-        }
-    ]
-    const updateTask=()=>{
-        updateListArray(obj, index)
-    }
-       const handleDelete=()=>{
-        deleteTask(index)
-    }
+export default function Card({
+  taskObj,
+  index,
+  taskList,
+  setTaskList,
+  setmodal,
+  modal,
+  toggle,
+  deleteTask,
+  updateListArray,
+}) {
+  const colors = [
+    {
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC",
+    },
+    {
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1",
+    },
+    {
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1",
+    },
+    {
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1F1",
+    },
+    {
+      primaryColor: "#B964F7",
+      secondaryColor: "#F3F0FD",
+    },
+  ];
+  const updateTask = (obj) => {
+    updateListArray(obj, index);
+  };
   return (
-    <div class = "card-wrapper mr-5">
-    <div class = "card-top" style={{"background-color": colors[index%5].primaryColor}}></div>
-    <div class = "task-holder">
-        <span class = "card-header" style={{"background-color": colors[index%5].secondaryColor, "border-radius": "10px"}}>{taskObj.Name}</span>
-        <p className = "mt-3">{taskObj.Description}</p>
+    <div className="card-wrapper mr-5">
+      <div
+        className="card-top"
+        style={{ "backgroundColor": colors[index % 5].primaryColor }}
+      ></div>
+      <div className="task-holder">
+        <span
+          className="card-header"
+          style={{
+            "backgroundColor": colors[index % 5].secondaryColor,
+            "borderRadius": "10px",
+          }}
+        >
+          {taskObj.Name}
+        </span>
+        <p className="mt-3">{taskObj.Description}</p>
 
-        <div style={{"position": "absolute", "right" : "20px", "bottom" : "20px"}}>
-            <i class = "far fa-edit mr-3" style={{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {() => setModal(true)}></i>
-            <i class="fas fa-trash-alt" style = {{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}></i>
+        <div style={{ position: "absolute", right: "20px", bottom: "20px" }}>
+          <i
+            className="far fa-edit mr-3"
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
+            onClick={() => setmodal(true)}
+          ></i>
+          <i
+            className="fas fa-trash-alt"
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
+            onClick={()=> deleteTask(index)
+            }
+          ></i>
         </div>
-</div>
-<Edittask modal = {modal} toggle = {toggle} updateTask = {updateTask} taskObj = {taskObj} />
-</div>
-  )
+      </div>
+      <Edittask
+        modal={modal}
+        toggle={toggle}
+        updateTask={updateTask}
+        taskObj={taskObj}
+      />
+    </div>
+  );
 }
--- src/components/Tasker.js
+++ src/components/Tasker.js
@@ -1,41 +1,66 @@
-import React , {useEffect ,useState} from 'react';
-import Card from './Card';
-import Createtask from './Createtask';
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Createtask from "./modal/Createtask";
 
 export default function Tasker() {
   const [modal, setmodal] = useState(false);
   const [taskList, setTaskList] = useState([]);
-  const toggle =()=>{
-    setmodal(!modal); 
-  } 
-  const deleteTask= (index)=>{
-    let tempList = taskList
-    tempList.slice(index ,1)
-    localStorage.getItem("taskLlist",JSON.stringify(tempList))
-    setTaskList(tempList)
-    window.location.reload()
-   }
+  const toggle = () => {
+    setmodal(!modal);
+  };
+  const deleteTask = (index) => {
+    let tempList = taskList;
+    tempList.slice(index, 1);
+    localStorage.getItem("taskLlist", JSON.stringify(tempList));
+    setTaskList(tempList);
+    window.location.reload();
+  };
+
+  const updateListArray = (obj, index) => {
+    let tempList = taskList;
+    tempList[index] = obj;
+    localStorage.getItem("taskList", JSON.stringify(tempList));
+    setTaskList(tempList);
+    window.location.reload();
+  };
 
   useEffect(() => {
-  let arr = localStorage.getItem("taskList")
-  if(arr){
-    let obj = JSON.parse(arr)
-    setTaskList(obj)
-  }
-  },[])
-  
+    let arr = localStorage.getItem("taskList");
+    if (arr) {
+      let obj = JSON.parse(arr);
+      setTaskList(obj);
+    }
+  }, []);
 
   return (
     <>
-    <div className='header text-center'>
-   <h3>Tasker</h3>
-   <button className='btn btn-primary mt-2' onClick={()=>setmodal(true)}>Create Task</button>
-    </div>
-    <div className="task-container">
-       {taskList.map((obj , index)=> <Card taskObj = {obj} index = {index} deleteTask={deleteTask}/>)}
-    </div>
-    <Createtask toggle={toggle} modal={modal} setmodal={setmodal} setTaskList={setTaskList} taskList={taskList}/>
-    
+      <div className="header text-center">
+        <h3>Tasker</h3>
+        <button className="btn btn-primary mt-2" onClick={() => setmodal(true)}>
+          Create Task
+        </button>
+      </div>
+      <div className="task-container">
+        {taskList.map((obj, index) => (
+          <Card
+            taskObj={obj}
+            index={index}
+            deleteTask={deleteTask}
+            updateListArray={updateListArray}
+            setmodal = {setmodal}
+            modal = {modal}
+            toggle = {toggle}
+            key={index}
+          />
+        ))}
+      </div>
+      <Createtask
+        toggle={toggle}
+        modal={modal}
+        setmodal={setmodal}
+        setTaskList={setTaskList}
+        taskList={taskList}
+      />
     </>
-  )
+  );
 }
